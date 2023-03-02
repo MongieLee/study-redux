942c91628ff76e57e93b5169d192ff1c573353b3
MongieLee <le30n@outlook.com>
2023-03-02 23:13:58 +0800

    feat: 引入redux-thunk替换手动实现的thunk

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,9 @@ import {applyMiddleware, legacy_createStore} from "redux";
 import reducers from "./reducers/index.js";
 import logMiddleware from "../middleware/logMiddleware.js";
 import thunkMiddleware from "../middleware/thunkMiddleware.js";
-
-const store = legacy_createStore(reducers, applyMiddleware(logMiddleware, thunkMiddleware));
+import thunk from "redux-thunk";
+// const store = legacy_createStore(reducers, applyMiddleware(logMiddleware, thunkMiddleware));
+const store = legacy_createStore(reducers, applyMiddleware(logMiddleware,thunk));
 store.subscribe(() => {
   console.log(`store更新了`);
   console.log(store.getState());
