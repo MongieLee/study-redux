cc1c526fe4cf21778fc60af899895d5e73934bf3
MongieLee <le30n@outlook.com>
2023-03-10 16:57:29 +0800

    feat: redux-actions中间件的使用

--- src/store/actions/count.actions.js
+++ src/store/actions/count.actions.js
@@ -1,7 +1,10 @@
 import {INCREMENT, DECREMENT, INCREMENT_SAGA} from "../const/count.const.js";
+import {createAction} from "redux-actions";
 
-export const increment = (payload = 1) => ({type: INCREMENT, payload});
-
-export const decrement = (payload = 1) => ({type: DECREMENT, payload});
+// export const increment = (payload = 1) => ({type: INCREMENT, payload});
+// export const decrement = (payload = 1) => ({type: DECREMENT, payload});
 
 export const increment_saga = (payload = 1) => ({type: INCREMENT_SAGA, payload});
+
+export const increment = createAction("increment");
+export const decrement = createAction("decrement");
--- src/store/reducers/count.reducers.js
+++ src/store/reducers/count.reducers.js
@@ -1,16 +1,30 @@
-import {DECREMENT, INCREMENT} from "../const/count.const.js";
+import {handleActions as createReducer} from "redux-actions";
+import {decrement, increment} from "../actions/count.actions.js";
 
 const initialValue = {
   count: 0
 }
 
-export default (state = initialValue, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return {count: state.count + action.payload}
-    case DECREMENT:
-      return {count: state.count - action.payload}
-    default:
-      return state;
-  }
-}
+const handleIncrement = (state, action) => {
+  return ({count: state.count + action.payload ?? 1})
+};
+
+const handleDecrement = (state, action) => ({count: state.count - action.payload ?? 1});
+
+const caReducer = createReducer({
+  [increment]: handleIncrement,
+  [decrement]: handleDecrement
+}, initialValue);
+
+export default caReducer;
+// export default (state = initialValue, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return {count: state.count + action.payload}
+//     case DECREMENT:
+//       return {count: state.count - action.payload}
+//     default:
+//       return state;
+//   }
+// }
+
